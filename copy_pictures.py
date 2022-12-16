a619168661761e28c108dd292fe6bc776c30adfe
tarneo <tarneo@tarneo.fr>
2022-12-16 22:41:10 +0100

    Add examples

---
+++ copy_pictures.py
@@ -0,0 +1,7 @@
+import lumix_control
+import os
+
+IP = "192.168.54.1" #IP of camera
+control = lumix_control.CameraControl(IP)
+
+control.get_pics(os.path.join(os.getenv("HOME"), "Pictures/pana")) #get pictures from camera and save them to ~/Pictures/pana
---
+++ live_view.py
@@ -0,0 +1,58 @@
+import lumix_control
+from PIL import Image, ImageTk
+import io
+import time
+import socket
+from tkinter import Tk, Label
+from threading import Thread
+
+#This gets more and more out of sync.
+
+IP = "192.168.54.1" #IP of camera
+control = lumix_control.CameraControl(IP)
+my_ip = "0.0.0.0"
+my_stream_port = 12345
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((my_ip, my_stream_port))
+    s.settimeout(10)
+    return s
+
+control.start_stream(my_stream_port)
+
+root = Tk()
+
+label = Label()
+label.pack()
+
+def daemon():
+    stream_socket = connect()
+    t = time.time()
+    while 1:
+        try:
+            data = stream_socket.recv(1024 * 512 * 1) # 0.5MB
+            data = data[data.find(b'\xff\xd8'):]
+        except socket.timeout:
+            control.start_stream(my_stream_port)
+            continue
+        if not data:
+            control.start_stream(my_stream_port)
+            continue
+        if time.time() - t > 5:
+            control.get_state()
+            t = time.time()
+        if data[-2:] != b'\xff\xd9':
+            print('no end of jpeg found from stream')
+            continue
+        img = Image.open(io.BytesIO(data))
+        img_widget = ImageTk.PhotoImage(img)
+        label.configure(image=img_widget)
+        label.image = img_widget
+        print("refreshed")
+
+
+Thread(target=daemon).start()
+
+root.mainloop()
+
