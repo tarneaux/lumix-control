22a30c983c7566f13f30b80bdaf643d74cde0b1e
tarneaux <tarneo@tarneo.fr>
2022-12-18 19:15:48 +0100

    Get ready for packaging the project

--- copy_pictures.py
+++
@@ -1,7 +0,0 @@
-import lumix_control
-import os
-
-IP = "192.168.54.1" #IP of camera
-control = lumix_control.CameraControl(IP)
-
-control.get_pics(os.path.join(os.getenv("HOME"), "Pictures/pana/raw/"), regex=".*\\..*") #get pictures from camera and save them to ~/Pictures/pana/raw/
