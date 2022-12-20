fee1e04efcee22ed3d816a71c0db945aa9f0e425
tarneaux <tarneo@tarneo.fr>
2022-12-20 18:10:43 +0100

    Remove conversion ability from copy

--- lumix_control/copy/__init__.py
+++ lumix_control/copy/__init__.py
@@ -4,8 +4,8 @@ from ..copy.regex import file_type_match
 import shutil
 import os
 
-def make_parser(download_parser: argparse.ArgumentParser):
-    conn_type = download_parser.add_subparsers(title='connection type', dest='conn_type')
+def make_parser(copy_parser: argparse.ArgumentParser):
+    conn_type = copy_parser.add_subparsers(title='connection type', dest='conn_type')
 
     wifi = conn_type.add_parser('wifi', help='Download files using wifi')
     usb = conn_type.add_parser('usb', help='Copy files using usb connection')
@@ -14,8 +14,7 @@ def make_parser(download_parser: argparse.ArgumentParser):
     add_args = lambda *args, **kwargs: wifi.add_argument(*args, **kwargs) and usb.add_argument(*args, **kwargs)
     add_args('-v', '--verbose', action='store_true', help='Print verbose output')
     add_args("-o", "--output", help="Output directory", required=True, type=str)
-    add_args("-e", "--extension", help="File extension to download", choices=['jpg', 'raw', 'image', 'mp4', 'all'], default='all', type=str)
-    add_args("-c", "--convert", help="Convert raw files to other formats", choices=['jpg', 'png'], default=None, type=str)
+    add_args("-e", "--extension", help="File extension to copy", choices=['jpg', 'raw', 'image', 'mp4', 'all'], default='all', type=str)
     add_args("-x", "--if-exists", help="What to do if file already exists", choices=['skip', 'overwrite', 'rename'], default='rename', type=str)
 
     # Arguments for wifi only
@@ -31,11 +30,8 @@ def main(args):
     colors.verbose = args.verbose
     debug = colors.debug
 
-    if args.convert:
-        check_darktable(debug)
-
     debug('Starting connection')
-    args.ip = start_connection(args.ip)
+    start_connection(args.ip if args.conn_type == 'wifi' else "")
     debug('Connection started')
 
     debug('Listing files on camera')
@@ -53,32 +49,14 @@ def main(args):
         total_size += file_size
         if not args.verbose:
             print("\33[2K\r", end='')
-            print(f'\rCopying {file.split("/")[-1]} ({i}/{len(files)}) ({human_readable_size(file_size)}) ', end='')
+            print(f'\rCopying {output_file.split("/")[-1]} ({i}/{len(files)}) ({human_readable_size(file_size)}) ', end='')
         debug(f'Copying {file} to {output_file}')
         copy_file(file, output_file)
     if not args.verbose:
         print("\33[2K\r", end='')
         print(f'Copied {len(files)} files ({human_readable_size(total_size)})')
     debug('Finished copying files')
-
-    if args.convert:
-        debug(f'Converting raw files to {args.convert}')
-        output_extension = args.convert
-        files_to_convert = [output_file for _, output_file in files if file_type_match('raw', output_file)]
-        for i, file in enumerate(files_to_convert):
-                if not args.verbose:
-                    print("\33[2K\r", end='')
-                    print(f'\rConverting {file.split("/")[-1]} ({i}/{len(files_to_convert)}) ', end='')
-                debug(f'Converting {file} to {file}')
-                output_file = os.path.splitext(file)[0] + f'.{output_extension}'
-                darktable_convert(file, output_file)
-        if not args.verbose:
-            print("\33[2K\r", end='')
-            print(f'Converted {len(files_to_convert)} files')
-        debug('Finished converting raw files')
-        end_connection()
-
-
+    end_connection()
 
 
 def human_readable_size(size: int):
@@ -88,15 +66,6 @@ def human_readable_size(size: int):
         size //= 1024
     return f'{size:.2f} PB'
 
-
-def check_darktable(debug):
-    debug(f'Verifying that darktable-cli is installed')
-    if shutil.which('darktable-cli') is None:
-        raise Exception('darktable-cli is not installed')
-    debug(f'darktable-cli is installed')
-
-def darktable_convert(input_file: str, output_file: str):
-    subprocess.call(['darktable-cli', input_file, output_file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 def filter_files(files: list, args: argparse.Namespace, debug, get_file_size, get_file_mtime, get_dest_name):
     debug(f'Filtering files by extension: {args.extension}')
